457a35c3ee57a37175244b930c8555cc0eae228e
Lorenxzo <imvinkelriedlorenzo@gmail.com>
2023-01-02 08:28:46 -0300

    test push in laptop

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -10,7 +10,7 @@ const nuevoNum = 20;
 const nuevoBool = true;
 
 // Resuelve el siguiente problema matemático:
-const nuevaResta = 10 - 5 === 5;
+const nuevaResta = 10 - 5 === 5; //probando 1 2 3
 
 // Resuelve el siguiente problema matemático:
 const nuevaMultiplicacion = 10 * 4 === 40 ;
